d037adfdf19c6b62c791becb87c90df68615a58b
Mimi <miraclef60@gmail.com>
2022-06-04 19:39:09 +0100

    CHORE: Refactored and added a new favicon to the application

--- apps/web/pages/_document.js
+++ apps/web/pages/_document.js
@@ -6,28 +6,16 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-          <link
-            rel="apple-touch-icon"
-            sizes="192x192"
-            href="/icon-192x192.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="256x256"
-            href="/icon-256x256.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="384x384"
-            href="/icon-384x384.png"
-          />
+          <link rel="apple-touch-icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="192x192" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="256x256" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="384x384" href="/favicon.png" />
 
           <link
             rel="icon"
             type="image/png"
             sizes="512x512"
-            href="/icon-512x512.png"
+            href="/favicon.png"
           />
           <meta name="theme-color" content="#fff" />
         </Head>
