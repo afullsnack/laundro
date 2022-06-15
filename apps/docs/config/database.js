0d9342185d468e5b7bd4a5f0ffccf6c7abf41d3a
Mimi <miraclef60@gmail.com>
2022-06-15 13:07:26 +0100

    CHORE: Added the pg-native library and modified the config/database.js file

--- apps/docs/config/database.js
+++ apps/docs/config/database.js
@@ -1,13 +1,20 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'laundro'),
-      user: env('DATABASE_USERNAME', 'mimi'),
-      password: env('DATABASE_PASSWORD', 'skeletonkey200'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "laundro"),
+      user: env("DATABASE_USERNAME", "mimi"),
+      password: env("DATABASE_PASSWORD", "skeletonkey200"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
+    pool: {
+      min: 0,
+      max: 0,
+      acquireTimeoutMillis: 10000,
+      createTimeoutMillis: 10000,
+    },
+    debug: true,
   },
 });
