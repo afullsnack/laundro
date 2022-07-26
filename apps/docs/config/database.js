623d7eec5c9d5df17691fe8a0473d2e78648a647
Mimi <miraclef60@gmail.com>
2022-07-26 16:12:42 +0100

    CHORE: Remodified google signup on local and added config var to the vercel deployment

--- apps/docs/config/database.js
+++ apps/docs/config/database.js
@@ -1,20 +1,24 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const config = process.env.DATABASE_URL
+  ? parse(process.env.DATABASE_URL)
+  : undefined;
 
 console.log("***READ FROM DEVELOPMENT***");
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", config.host || "127.0.0.1"),
-      port: env.int("DATABASE_PORT", config.port || 5432),
-      database: env("DATABASE_NAME", config.database || "laundro"),
-      user: env("DATABASE_USERNAME", config.user || "mimi"),
-      password: env("DATABASE_PASSWORD", config.password || "skeletonkey200"),
-      // ssl: env.bool("DATABASE_SSL", false),
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env("DATABASE_HOST", config?.host || "127.0.0.1"),
+      port: env.int("DATABASE_PORT", config?.port || 5432),
+      database: env("DATABASE_NAME", config?.database || "laundro"),
+      user: env("DATABASE_USERNAME", config?.user || "mimi"),
+      password: env("DATABASE_PASSWORD", config?.password || "skeletonkey200"),
+      ssl:
+        process.env.NODE_ENV === "development"
+          ? env.bool("DATABASE_SSL", false)
+          : {
+              rejectUnauthorized: false,
+            },
     },
     // pool: {
     //   min: 0,
